9b2fc575fa1692b50ed12787076a078ee43aa917
gtaka-1 <gtaka1310@gmail.com>
2026-09-19 18:27:25 -0300

    Create seq.py

---
+++ seq.py
@@ -0,0 +1,17 @@
+import time
+
+def contagem_pesada(n):
+    while n > 0:
+        n -= 1
+
+if __name__ == "__main__":
+    n = 100_000_000
+
+    inicio = time.time()
+
+    contagem_pesada(n)
+    contagem_pesada(n)
+
+    fim = time.time()
+
+    print(f"[Python Sequencial] Tempo: {fim - inicio:.4f} segundos")
